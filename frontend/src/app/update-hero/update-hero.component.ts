15b3a0cf7f9e338af02933b6b49c0e61f27851b9
James Meegan <jmeegan2@owlmail.harford.edu>
2022-03-29 20:21:51 -0400

    form tag on html create not working right

--- frontend/src/app/update-hero/update-hero.component.ts
+++ frontend/src/app/update-hero/update-hero.component.ts
@@ -12,14 +12,14 @@ import { Router } from '@angular/router';
 export class UpdateHeroComponent implements OnInit {
 
   @Input() hero: Partial<Hero> = {};
-  name:string | null = '';
+  name: string | null = '';
   id: string | null = '';
 
   constructor(
     private route: ActivatedRoute,
     private backendService: BackendService,
     private router: Router
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
@@ -30,40 +30,38 @@ export class UpdateHeroComponent implements OnInit {
   //  I just took this from hero-details component, thats what I used
   // this function for
 
-  async getHero(): Promise<void>{
+  async getHero(): Promise<void> {
     this.id = this.route.snapshot.paramMap.get("id"), 10;
     this.hero = await this.backendService.getTheHero(this.id);
-}
-//The three functions below are responsible for updating the hero with
-//the new class, name, and level.
+  }
+  //The three functions below are responsible for updating the hero with
+  //the new class, name, and level.
 
-updatedClassValue(heroClass: string){
-  this.hero.class = heroClass;
-}
-updatedNameValue(heroName: string){
-  this.hero.name = heroName;
-}
-updatedLevelValue(heroLevel: number){
-  this.hero.level = +heroLevel;
-}
+  updatedClassValue(heroClass: string) {
+    this.hero.class = heroClass;
+  }
+  updatedNameValue(heroName: string) {
+    this.hero.name = heroName;
+  }
+  updatedLevelValue(heroLevel: number) {
+    this.hero.level = +heroLevel;
+  }
 
-//Responsible for updating the hero and sending the data to the backend 
+  //Responsible for updating the hero and sending the data to the backend 
 
-    async updateHero(): Promise<void>{
-  await this.backendService.updateTheHero(String(this.id),this.hero)
+  async updateHero(): Promise<void> {
+    await this.backendService.updateTheHero(String(this.id), this.hero)
   }
 
   // This is what I use to run the updateHero function and take me back
   // to the main page on click of the button
 
-  routerToHeroList()
-
-    {
-    if(confirm("Has the hero been updated correctly?")) {
-     this.updateHero(); //create hero
-    this.router.navigate(['/']); //main page
-     }
+  routerToHeroList() {
+    if (confirm("Has the hero been updated correctly?")) {
+      this.updateHero(); //create hero
+      this.router.navigate(['/']); //main page
     }
+  }
 
 }
 
